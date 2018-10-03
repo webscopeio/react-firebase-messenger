ae43447102cc291720d7684b48843e544b653ad5
Dmytro <dmytro.ilhanaiev@gmail.com>
2018-10-03 13:18:13 +0200

    Added firebase calls

---
+++ src/firebase/references.js
@@ -0,0 +1,33 @@
+export const usersRef = (
+  firebaseDB,
+  userId,
+) => firebaseDB.child(`users/${userId}`)
+
+export const chatMessagefRef = (
+  firebaseDB,
+  chatId
+) => firebaseDB.child(`chat-messages/${chatId}`)
+
+export const chatMetadataRef = (
+  firebaseDB,
+  chatId
+) => firebaseDB.child(`chat-metadata/${chatId}`)
+
+export const userEventChatsRef = (
+  firebaseDB,
+  userId,
+  eventId,
+  chatId
+) => firebaseDB.child(`user-event-chats/${userId}/${eventId}/${chatId}`)
+
+export const userEventAllChatsRef = (
+  firebaseDB,
+  userId,
+  eventId
+) => firebaseDB.child(`user-event-chats/${userId}/${eventId}`)
+
+export const userChatsRef = (
+  firebaseDB,
+  userId,
+  chatId
+) => firebaseDB.child(`user-chats/${userId}/${chatId}`)
