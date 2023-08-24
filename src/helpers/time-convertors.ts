fc22edcedb56798efc823b0082dfa2c8fd6fc65e
samuelgocnik <gocnik@webscope.io>
2023-08-24 18:15:55 +0200

    fix: time converting

--- src/helpers/time-convertors.ts
+++ src/helpers/time-convertors.ts
@@ -1,5 +1,5 @@
 import dayjs from "dayjs";
 
-export const toUnixTimestamp = (item: Date): string => dayjs(item).format("x");
+export const toUnixTimestamp = (item: Date): string => dayjs(item).unix().toString();
 
-export const unixToJSDate = (time: string): Date => dayjs(time, "x").toDate();
+export const unixToJSDate = (time: string): Date => dayjs.unix(+time).toDate();
