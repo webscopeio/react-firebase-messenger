7fb9726ffb7554871189c40635f527b65ac4bd13
samuelgocnik <gocnik@webscope.io>
2023-08-31 15:11:24 +0200

    fix: added exports

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,2 +1,38 @@
-export const bar = () => 'bar'
+import type {
+  ChatMetadata,
+  ChatMessage,
+  ChatUser,
+  UserChats,
+  UserChatsEntity,
+  Message,
+  CollectionObject,
+  UnreadMessage,
+  ChatParticipant,
+} from './models/database'
+import { toUnixTimestamp, unixToJSDate } from './helpers/time-convertors'
+import {
+  transformMessagesToStoreInDB,
+  loadMoreMessagesListTransform,
+  listnerSingleMessageTransform,
+  toFlatList,
+  getMessagesIds,
+} from './helpers/transformations'
 
+export {
+  type ChatMetadata,
+  type ChatMessage,
+  type ChatUser,
+  type UserChats,
+  type UserChatsEntity,
+  type Message,
+  type CollectionObject,
+  type UnreadMessage,
+  type ChatParticipant,
+  toUnixTimestamp,
+  unixToJSDate,
+  transformMessagesToStoreInDB,
+  loadMoreMessagesListTransform,
+  listnerSingleMessageTransform,
+  toFlatList,
+  getMessagesIds,
+}
