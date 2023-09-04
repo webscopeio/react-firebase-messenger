fe724e9fa27858001192811143ee1fc776e785c7
samuelgocnik <gocnik@webscope.io>
2023-09-04 15:20:14 +0200

    feat: added index.ts

--- packages/native/index.ts
+++ packages/native/index.ts
@@ -1,2 +1,39 @@
-// Add the exports here
-export const foo = (): string => 'foo' + 1
+import {
+  toSendMessage,
+  createEmptyChat,
+  addUserToChat,
+  checkForChatExistence,
+  getGroupChatsByEvent,
+  getChatById,
+} from './helpers/calls'
+import {
+  userChatsRef,
+  usersRef,
+  chatMessagesRef,
+  chatMetadataRef,
+  userEventChatsRef,
+  userEventAllChatsRef,
+  userChatsAllRef,
+  allChatMetadataRef,
+} from './helpers/references'
+import ChatProviderWrapper from './components/ChatWindowProvider'
+import ChatListProvider from './components/ChatListProvider'
+
+export {
+  toSendMessage,
+  createEmptyChat,
+  addUserToChat,
+  checkForChatExistence,
+  getGroupChatsByEvent,
+  getChatById,
+  userChatsRef,
+  usersRef,
+  chatMessagesRef,
+  chatMetadataRef,
+  userEventChatsRef,
+  userEventAllChatsRef,
+  userChatsAllRef,
+  allChatMetadataRef,
+  ChatProviderWrapper,
+  ChatListProvider,
+}
